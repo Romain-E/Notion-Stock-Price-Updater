2d48a9e85b8bd0af929616aa0d18feef148108de
Romain Emery <romain.emery78@gmail.com>
2024-03-29 12:41:45 +0100

    [~] Github Actions first launch

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from notion import NotionClass
 from yahooFinance import YahooFinance
 
 class Main:
-    def __init__(self, config_file):
-        self.config_file = config_file
-        self.config_data = self.load_config()
+    def __init__(self):
+        self.notion_token = os.environ.get('NOTION_TOKEN')
+        self.datanase_id = os.environ.get('DATABASE_ID')
         self.headers = self.initialize_headers()
 
     def load_config(self):
@@ -16,7 +16,7 @@ class Main:
 
     def initialize_headers(self):
         headers = {
-            "Authorization": "Bearer " + self.config_data.get('notion_token'),
+            "Authorization": "Bearer " + self.notion_token,
             "Content-Type": "application/json",
             "Notion-Version": "2022-06-28",
         }
@@ -26,7 +26,7 @@ class Main:
         print("=== Lancement de l'application ===")
 
         # Init
-        notion = NotionClass(database_id=self.config_data.get('database_id'), headers=self.headers)
+        notion = NotionClass(database_id=self.datanase_id, headers=self.headers)
         yahooFinance = YahooFinance()
 
         # Récupération des pages 
